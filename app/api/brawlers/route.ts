bee31eafea7c7257da85a5f26cb4e263c2106db1
yoshiki-maruya <yoshiki.maruya@gmail.com>
2024-05-27 00:08:27 +0900

    [impr]: create brawlers api

---
+++ app/api/brawlers/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    const response = await fetch('https://api.brawlstars.com/v1/brawlers', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.BRAWL_API_KEY}`,
+      },
+    });
+
+    // レスポンスが成功かどうかを確認
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 })
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
